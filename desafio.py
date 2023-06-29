7a16378d3ecd8687042000481dd8220fec449b0c
Raimundo Azevedo <raaznt@gmail.com>
2023-06-29 16:52:12 -0300

    Updated: desafio.py refatorado com o uso de funcoes

--- desafio.py
+++ desafio.py
@@ -1,3 +1,18 @@
+menu = """
+=============== MENU ===============
+[u] \tCriar Usuario
+[c] \tCriar Conta
+[d] \tDepositar
+[s] \tSacar
+[e] \tExtrato
+[lu] \tListar Usuarios
+[lc] \tListar Contas
+[q] \tSair
+====================================
+
+=> """
+
+
 def check_value(n):
     is_valid = False
     value = None
@@ -10,62 +25,148 @@ def check_value(n):
     return is_valid, value
 
 
-menu = """
-
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
-
-
-=> """
-
-saldo = 0
-limite = 500
-extrato = ""
-numero_saques = 0
-on_loop = True
-LIMITE_SAQUES = 3
-
-while on_loop:
-    opcao = input(menu)
-
-    if opcao == "d":
-        print("Operacao de Deposito foi selecionada.")
-        deposito = input("Valor a depositar: ")
-        is_valid, deposito = check_value(deposito)
-        if is_valid:
-            if deposito <= 0:
-                print("Operação não realizada. Valor inválido.")
-            else:
-                saldo += deposito
-                extrato += f"+ R$ {deposito:.2f}\n"
-                print("Operacao concluída.")
+def depositar(saldo, valor, extrato, /):
+    is_valid, valor = check_value(valor)
+    if is_valid:
+        if valor <= 0:
+            print("Operação não realizada. Valor inválido.")
         else:
-            print("Valor informado nao eh numérico.")
-
-    elif opcao == "s":
-        print("Operacao de Saque foi selecionada.")
-        saque = input("Informe um valor: ")
-        is_valid, saque = check_value(saque)
-        if is_valid:
-            if numero_saques == LIMITE_SAQUES:
-                print("Operacao nao realizada. Limite de 3 saques diarios.")
-            else:
-                if saque > 500.00:
-                    print("Operacao nao realizada. Limite de R$ 500.00 por saque.")
-                elif saque < 0:
-                    print("Valor informado eh invalido")
-                elif saldo - saque < 0:
-                    print("Saldo insuficiente.")
-                else:
-                    saldo -= saque
-                    extrato += f"- R$ {saque:.2f}\n"
-                    numero_saques += 1
-                    print("Operação concluida")
-
-    elif opcao == "e":
-        print(f"Extrato\n\n{extrato}\nSALDO: {saldo:.2f}\n")
-
+            saldo += valor
+            extrato += f"+ R$ {valor:.2f}\n"
+            print("Operacao concluída.")
     else:
-        on_loop = False
+        print("Valor informado nao eh numérico.")
+
+    return saldo, extrato
+
+
+def sacar(
+    saldo=0,
+    valor=0,
+    extrato=None,
+    limite=500,
+    numero_saques=0,
+    limite_saques=3,
+):
+    is_valid, valor = check_value(valor)
+    if is_valid:
+        if numero_saques == limite_saques:
+            print("Operacao nao realizada. Limite de 3 saques diarios.")
+        else:
+            if valor > limite:
+                print(f"Operacao nao realizada. Limite de R$ {limite} por saque.")
+            elif valor < 0:
+                print("Valor informado eh invalido")
+            elif saldo - valor < 0:
+                print("Saldo insuficiente.")
+            else:
+                saldo -= valor
+                extrato += f"- R$ {valor:.2f}\n"
+                numero_saques += 1
+                print("Operação concluida")
+    return saldo, extrato
+
+
+def exibir_extrato(saldo, /, extrato=""):
+    print(f"Extrato\n\n{extrato}\nSALDO: {saldo:.2f}\n")
+
+
+def criar_usuario(lista: list, nome, data_nascimento, cpf, endereco):
+    usuario = {}
+    usuario["nome"] = nome
+    usuario["data_nascimento"] = data_nascimento
+    usuario["cpf"] = cpf
+    usuario["endereco"] = endereco
+
+    found = False
+    for cliente in lista:
+        if cliente.get("cpf") == cpf:
+            found = True
+            break
+
+    if not found:
+        lista.append(usuario)
+    else:
+        print("Ja ha cliente com o CPF informado cadastrado")
+
+
+def exibir_lista(lista: list):
+    for usuario in lista:
+        print(usuario, end="\n")
+
+
+def criar_conta(lista_contas: list, lista_clientes: list, cpf_usuario):
+    conta = {}
+    conta["agencia"] = "0001"
+    conta["nro_conta"] = (
+        int(lista_contas[-1].get("nro_conta")) + 1 if lista_contas else 1
+    )
+    conta["cpf_usuario"] = cpf_usuario
+
+    found = False
+    for cliente in lista_clientes:
+        if cliente.get("cpf") == cpf_usuario:
+            found = True
+            break
+
+    if found:
+        lista_contas.append(conta)
+    else:
+        print("Usuario nao cadastrado")
+
+
+def main():
+    saldo = 0
+    limite = 500
+    extrato = ""
+    numero_saques = 0
+    on_loop = True
+    LIMITE_SAQUES = 3
+
+    usuarios = []
+    contas = []
+    while on_loop:
+        opcao = input(menu)
+
+        if opcao == "d":
+            print("Operacao de Deposito foi selecionada.")
+            deposito = input("Valor a depositar: ")
+            saldo, extrato = depositar(saldo, deposito, extrato)
+
+        elif opcao == "s":
+            print("Operacao de Saque foi selecionada.")
+            saque = input("Informe um valor: ")
+            saldo, extrato = sacar(
+                saldo=saldo,
+                valor=saque,
+                extrato=extrato,
+                limite=500,
+                numero_saques=numero_saques,
+                limite_saques=LIMITE_SAQUES,
+            )
+
+        elif opcao == "e":
+            exibir_extrato(saldo, extrato=extrato)
+
+        elif opcao == "u":
+            nome = input("Informe nome: ")
+            data_nascimento = input("Informe data de nascimento: ")
+            cpf = input("Informe cpf: ")
+            endereco = input("Informe endereco: ")
+            criar_usuario(usuarios, nome, data_nascimento, cpf, endereco)
+
+        elif opcao == "lu":
+            exibir_lista(usuarios)
+
+        elif opcao == "c":
+            cpf = input("Informe cpf do usuario: ")
+            criar_conta(contas, usuarios, cpf)
+
+        elif opcao == "lc":
+            exibir_lista(contas)
+        else:
+            on_loop = False
+
+
+if __name__ == "__main__":
+    main()
